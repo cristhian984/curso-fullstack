da4c625b19af5f297061919634f238939b88b601
cristhian984 <cristhiandosreisvieira@gmail.com>
2026-05-19 14:59:41 -0300

    feat: add codigo

--- kPfgi/app.js
+++ kPfgi/app.js
@@ -1,11 +1,34 @@
 const express = require ("express");
 const app = express();
-const port = 3001;
+const port = 3000;
+
+//Variavel com os dados
+const alimentos = [
+{id: 1, nome: "Feijão", preco:10},
+{id: 2, nome: "Carne", preco:50},
+{id: 3, nome: "Frutas", preco:70},
+{id: 4, nome: "Sopas", preco:40}
+
+];
+
+const tiposPratos = [
+  {id: 1, nome: "prato executivo", preco:10},
+{id: 2, nome: "prato Principal", preco:50},
+{id: 3, nome: "Sobremesa", preco:70},
+];
+
+//Produtos
+app.get ('/produtos',(req,res) =>{
+res.send(alimentos)
+});
+app.get ('/cardapio', (req,res) =>{
+   res.send(tiposPratos)
+});
 
 //criar um função
 //rota principal
 app.get('/', (req,res) => {
-   res.send("Bem Vindo ao meu projeto de alimentos"); 
+   res.send("Bem Vindo ao meu projeto de alimentos do Kigut e Cia"); 
 });
 
 //Executando um servidor
