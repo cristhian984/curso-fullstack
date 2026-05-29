2c0449a955fbf720372296ed21b27ecd1c9246ed
cristhian984 <cristhiandosreisvieira@gmail.com>
2026-05-29 16:17:53 -0300

    feat:add

--- copa-do-mundo-node/infra/conexao.js
+++ copa-do-mundo-node/infra/conexao.js
@@ -1,11 +1,13 @@
-import mysql from "mysq12";
+import mysql from "mysql2";
+
 const conexao = mysql.createConnection({
-    host: 'locahost',
+    host: 'localhost',
     port: '3306',
     user: 'root',
-    password:'root',
-    database:'copa_mundo'
+    password: 'root',
+    database: 'copa_mundo'
 });
 
 conexao.connect();
+
 export default conexao;
--- copa-do-mundo-node/src/app.js
+++ copa-do-mundo-node/src/app.js
@@ -4,20 +4,62 @@ const app = express();
 
 // Indicar para express ler o body com json
 app.use(express.json());
-import conexao from "../infra/conexao.js"
+import conexao from "../infra/conexao.js";
 
-// Rota Principal
+
+// Rota principal
 app.get("/", (req, res) => {
     res.send("Olá copa do mundo!");
 });
 
-// Buscar todas as seleções
+// Buscar todas selecões
 app.get('/selecoes', (req, res) => {
-    const sql = "select * from selecoes"
+    const sql = "select * from selecoes";
 
     conexao.query(sql, (erro, result) => {
         res.json(result);
     });
 });
+
+//busncando por Id
+app.get('/selecoes/:id', (req, res) => {
+    const id = req.params.id
+    const sql = "select * from selecoes where id=?;";
+
+    conexao.query(sql, id, (erro, result) => {
+        res.json(result[0]);
+
+    });
+})
+//criando POST para cadastrar
+app.post('/selecoes', (req, res) => {
+    const selecao = req.body;
+    const sql = "insert into selecoes set?";
+    conexao.query(sql, selecao, () => {
+        res.json({ mensagem: "Cadastrado com sucesso!" });
+
+    });
+});
+//deletando dados
+app.delete('/selecoes/:id',(req, res) => {
+    const id = req.params.id
+    const sql = "delete from selecoes where id=?";
+
+    conexao.query(sql, id, () => {
+        res.json(["deletado com sucesso"])
+    });
+});
+
+//atualizando registro
+app.put ('/selecoes/:id', (req,res) => {
+    const id = req.params.id;
+    const selecao = req.body;
+    const sql = "update selecoes set ? where id=?";
+
+    conexao.query(sql, [selecao, id], () => {
+        res.json({mensagem: "Atualizando com sucesso!" });
+    });
+});
+
 
 export default app;
